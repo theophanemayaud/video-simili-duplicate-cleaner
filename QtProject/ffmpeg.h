90fb352141182d7ec1294b949c285e74a2da6284
Théophane Mayaud <theophane.mayaud@gmail.com>
2021-03-05 17:45:23 +0100

    chore: clean debug messages and leftover mess

--- QtProject/ffmpeg.h
+++ QtProject/ffmpeg.h
@@ -10,22 +10,6 @@ extern "C" {
 
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
-
-//#include "libavformat/avformat.h"
-//#include "libavutil/dict.h"
-
-//#include "libavcodec/avcodec.h"
-//#include "libavformat/avformat.h"
-//#include <libavutil/dict.h>
-//#include "libavformat/riff.h"
-//#include "libavformat/metadata.h"
-//#include "libavformat/utils.h"
-//#include "libavcodec/opt.h"
-//#include "libavutil/rational.h"
-//#include "options.h"
-//#include "libavutil/avstring.h"
-//#include "libavutil/internal.h"
-//#include "libswscale/swscale.h"
 }
 }
 
