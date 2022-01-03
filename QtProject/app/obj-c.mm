ce6f579c30650c43f0c8b7d408280d769a1a8605
Théophane Mayaud <farineux.poilu0h@icloud.com>
2022-01-03 16:54:56 +0100

    feat(apple photos): reveal and play in apple photos instead of file manager if in Photos library

--- QtProject/app/obj-c.mm
+++ QtProject/app/obj-c.mm
@@ -20,7 +20,7 @@ char *Obj_C::obj_C_addMediaToAlbum(char *albumName, char *mediaId)
     NSAppleEventDescriptor *resultDesc = [script executeAndReturnError:&errorDictionary];
 
     NSString *returnString = @OBJ_C_SUCCESS_STRING;
-    if ( resultDesc ) {
+    if ( resultDesc ) { // was successful
         return (char *)[returnString UTF8String];
     }
     else{
@@ -44,11 +44,36 @@ char *Obj_C::obj_C_getMediaName(char *mediaId)
     NSAppleEventDescriptor *resultDesc = [script executeAndReturnError:&errorDictionary];
 
     NSString *returnString = @OBJ_C_FAILURE_STRING;
-    if ( !resultDesc ) {
+    if ( !resultDesc ) { // failed
         return (char *)[returnString UTF8String];
     }
     else{
         returnString = [NSString stringWithFormat:@"%@", resultDesc.stringValue];
+        return (char *)[returnString UTF8String];
+    }
+}
+
+char *Obj_C::obj_C_revealMediaInPhotosApp(char *mediaId)
+{
+    NSString *mediaIdS = [NSString stringWithUTF8String: mediaId];
+
+    NSString *source = [NSString stringWithFormat:@"tell application \"Photos\"\n"
+                                                  @"    set selMedia to (get media items whose id contains \"%@\")\n"
+                                                  @"    spotlight item 1 of selMedia\n"
+                                                  @"    activate\n"
+                                                  @"end tell", mediaIdS];
+
+    NSDictionary *errorDictionary;
+    NSAppleScript *script = [[NSAppleScript alloc] initWithSource:source];
+
+    NSAppleEventDescriptor *resultDesc = [script executeAndReturnError:&errorDictionary];
+
+    NSString *returnString = @OBJ_C_SUCCESS_STRING;
+    if ( resultDesc ) { // was successful
+        return (char *)[returnString UTF8String];
+    }
+    else{ // was an error
+        returnString = [NSString stringWithFormat:@"%@", errorDictionary];
         return (char *)[returnString UTF8String];
     }
 }
