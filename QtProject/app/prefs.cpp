86310d9f4d4e33aca9daf5e16054864d6cfee5e5
Théophane Mayaud <soleil.ulnaire-0d@icloud.com>
2025-10-11 10:50:29 +0200

    chore(tests): add cmake tests (#161)

---
+++ QtProject/app/prefs.cpp
@@ -0,0 +1,5 @@
+// This file exists to allow Qt MOC to generate meta-object code for classes in prefs.h
+// The Message class uses Q_OBJECT and signals, so MOC needs a .cpp file to process
+#include "prefs.h"
+
+
--- QtProject/tests/test_video/video_test_helpers.cpp
+++ QtProject/tests/test_video/video_test_helpers.cpp
@@ -1,3 +1,4 @@
+#include "video_test_helpers.h"
 #include <QTest>
 #include <QCoreApplication>
 #include <QWidget>
@@ -5,38 +6,8 @@
 #include <QLabel>
 #include <QCheckBox>
 
-class VideoParam {
-public:
-    static const int nb_params = 13;
-    static const char sep = '"';
-    static const QString timeformat;
-
-    QFileInfo videoInfo;
-    QFileInfo thumbnailInfo;
-    int64_t size;
-    QDateTime modified;
-    int64_t duration;
-    int bitrate;
-    double framerate;
-    QString codec;
-    QString audio;
-    short width;
-    short height;
-    uint64_t hash1;
-    uint64_t hash2;
-};
-
+// Define static member
 const QString VideoParam::timeformat = "yyyy-MM-dd HH:mm:ss";
-
-class TestHelpers {
-public:
-    static bool doThumbnailsLookSameWindow(const QByteArray ref_thumb, const QByteArray new_thumb, const QString title);
-    // returns an empty list if either csv didn't exist, or something else happened.
-    static QList<VideoParam> importCSVtoVideoParamQList(const QFileInfo csvInfo, const QDir videoDir, const QDir thumbDir); // CSV : sep  seperated values
-
-    // will not overwrite it already exists
-    static bool saveVideoParamQListToCSV(const QList<VideoParam> videoParamQList, const QFileInfo csvInfo, const QDir videoBaseDir); // false->failed to save
-};
 
 // -------------------------------------------------
 // ------------START TestHelpers -----------------
